e344dd5e8dcca0e221ab8fea6f90820d7acf0b85
mrsaif786 <saifaliraja57@gmail.com>
2026-05-08 00:58:22 +0530

    Enhance chat functionality and update home route

--- app.py
+++ app.py
@@ -1,14 +1,28 @@
-from flask import Flask
+from flask import Flask, request, jsonify, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "🔥 ZOYA AI RUNNING SUCCESSFULLY"
+    return render_template("index.html")
 
 @app.route("/chat", methods=["POST"])
 def chat():
-    return {"reply": "Hello! Main Zoya AI hoon 🤖"}
+
+    user_msg = request.json["message"].lower()
+
+    if "hello" in user_msg:
+        reply = "Hello! Main Zoya AI hoon 🤖"
+
+    elif "tum kaise ho" in user_msg:
+        reply = "Main theek hoon 😊"
+
+    else:
+        reply = "Mujhe aur train karo 🤖"
+
+    return jsonify({
+        "reply": reply
+    })
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
